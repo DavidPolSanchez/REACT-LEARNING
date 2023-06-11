48e8882088c09e1f3112ea3d9648d2c5074fc9ee
David Pol <davidpolsanchezmartos@gmail.com>
2023-06-11 21:32:32 +0200

    :sparkles: Add views and JSX
    add componentes for course section 7

---
+++ 01-primeros-pasos/src/SegundoComponente.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+  const SegundoComponente = () => {
+    const libros =['Harry Potter','Juego de Tronos','CleanCode'];
+   /* ['Harry Potter','Juego de Tronos','CleanCode'];*/
+  return (
+    <div className='segundo-componente'>
+        <h1>Listado de Libros</h1>
+        <hr/>
+        {libros.length >= 1 ?
+            (<ul>
+                {
+                    libros.map( (libro ,indice) =>{
+                        return <li key ={indice}>{libro}</li>
+                    })   
+                }   
+            </ul>) 
+            :
+            (<p>No hay libros</p>)
+        }
+
+    </div>
+  )
+}
+ export default SegundoComponente;
