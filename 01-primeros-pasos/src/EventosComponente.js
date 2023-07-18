4ec7623548a75d6e41c522fc2851d993f8d9e8f8
David Pol <davidpolsanchezmartos@gmail.com>
2023-07-18 12:41:38 +0200

    :sparkles: More events example
    double,mouse, focus

--- 01-primeros-pasos/src/EventosComponente.js
+++ 01-primeros-pasos/src/EventosComponente.js
@@ -6,12 +6,57 @@ export const EventosComponente = () => {
         alert("Has Dado Click"+nombre);
     }
 
+    const handlerHasDadoDobleClick = (e) => {
+        alert("Has Dado Docble click")
+    }
+
+    const hasEntrado= (e,accion)=>{
+      console.log(`has ${accion}a la caja del mouse`);
+    }
+
+    const estasDentro = e => {
+      console.log("Estas Dentro")
+    }
+    const estasFuera = e => {
+      console.log("EstasFuera")
+    }
+
 
   return (
     <div>
         <h1>Eventos de React </h1>
-        {/*Evento CLick*/}
-        <button onClick={e =>HandlerHasDadoClick(e,"david Pol ") }>Dame Click</button>
-        </div>
+
+        <p>
+          {/*Evento CLick*/}
+          <button onClick={e =>HandlerHasDadoClick(e,"david Pol ") }>Dame Click</button>          
+        </p>
+
+        <p>
+          {/*Evento Doble CLick*/}
+          <button onDoubleClick={handlerHasDadoDobleClick }>Dame doble Click</button>  
+        </p>
+
+        <p>
+          {/*Eventos OnMouse*/}
+          <div id="caja" 
+          
+            onMouseEnter={e => hasEntrado(e,"entrado")} 
+            onMouseLeave={e => hasEntrado(e,"salido")}
+            >
+            Pasa por encima 
+          </div>
+        </p>
+        <p>
+          {/*Eventos OnFocus/blur*/}
+          <input type="text" 
+          
+            onFocus={estasDentro} 
+            onMouseLeave={estasFuera}
+            placeholder="Introduce un texto"
+            >
+            
+          </input>
+        </p>
+      </div>
   )
 }
