9e95b78a8cace5118f0cfef61e2df96d81d156ce
David Pol <davidpolsanchezmartos@gmail.com>
2023-07-26 09:07:20 +0200

    :construction: Working on use Effect
    not finnish

---
+++ 03-use-effect/src/components/PruebasComponent.js
@@ -0,0 +1,16 @@
+
+import React, { useState} from 'react'
+
+export const PruebasComponent = () => {
+
+    const [usuario,setusuario]= useState["federico rico"]
+
+  return (
+    <div>
+        <h1>El Efecto -Hooks UseEffect</h1>
+        <form>
+            <input type="text" />
+        </form>
+    </div>
+  )
+}
