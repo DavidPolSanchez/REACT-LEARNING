07d0009a6a888d61cee3f896525e061eaaa0849d
David Pol <davidpolsanchezmartos@gmail.com>
2023-07-26 09:08:17 +0200

    :construction_worker: Change Pruebas component

--- 03-use-effect/src/components/PruebasComponent.js
+++ 03-use-effect/src/components/PruebasComponent.js
@@ -5,11 +5,12 @@ export const PruebasComponent = () => {
 
     const [usuario,setusuario]= useState["federico rico"]
 
+    const modusuario
   return (
     <div>
         <h1>El Efecto -Hooks UseEffect</h1>
         <form>
-            <input type="text" />
+            <input  type="text" onChange={modusuario} />
         </form>
     </div>
   )
